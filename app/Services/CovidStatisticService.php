d4177e93e4034d4abfaf892e1cfb43e8aa564ffa
MandalorianFighter <yanush.polishchuk@gmail.com>
2023-01-10 16:49:38 +0200

    fix CovidStatisticService

--- app/Services/CovidStatisticService.php
+++ app/Services/CovidStatisticService.php
@@ -10,10 +10,10 @@ class CovidStatisticService
     public function getTotalCasesByCountryAndType($country, $type)
     {
         $today = Carbon::now()->toDateString();
-
+        
         $request = Http::get("https://api.covid19api.com/total/country/{$country}/status/{$type}?from={$today}&to={$today}");
 
-        $response = json_decode($request->getBody()->getContents());
+        $response = json_decode($request->body());
 
         return $response[count($response) - 1];
     }
