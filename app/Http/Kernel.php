349745fa6bd696d180435f8652ed24d62630e448
MandalorianFighter <yanush.polishchuk@gmail.com>
2023-01-10 16:50:17 +0200

    add varnish

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,6 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'cacheable' => \Spatie\Varnish\Middleware\CacheWithVarnish::class,
     ];
 }
